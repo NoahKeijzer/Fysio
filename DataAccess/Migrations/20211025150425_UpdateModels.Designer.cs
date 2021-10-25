37ee9512f08b94bb1ac6f5e390e962b3cb7c6c35
Noah de Keijzer <noah.cristiaan@gmail.com>
2021-10-25 17:05:37 +0200

    Update Database

--- DataAccess/Migrations/20211025150425_UpdateModels.Designer.cs
+++ DataAccess/Migrations/20211025150425_UpdateModels.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fysio.Migrations
 {
     [DbContext(typeof(FysioDbContext))]
-    partial class FysioDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211025150425_UpdateModels")]
+    partial class UpdateModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccess/Migrations/20211025150425_UpdateModels.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fysio.Migrations
+{
+    public partial class UpdateModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DiagnosOmschrijving",
+                table: "Patiëntdossiers",
+                newName: "DiagnoseOmschrijving");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DiagnoseCode",
+                table: "Patiëntdossiers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DiagnoseOmschrijving",
+                table: "Patiëntdossiers",
+                newName: "DiagnosOmschrijving");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DiagnoseCode",
+                table: "Patiëntdossiers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
